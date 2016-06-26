bd6089afa6e38d93adc6d5d11887d40df316ff93
nverno <noah.v.peart@gmail.com>
2016-06-26 16:22:36 -0400

    template

--- cmd-mode.el
+++ cmd-mode.el
@@ -415,7 +415,7 @@ be a variable name, it usually isn't."
   "Insert minimal batch file template."
   (interactive)
   (goto-char (point-min))
-  (insert "@echo off\nsetlocal enableextensions\n\n"))
+  (insert "@echo off\nsetlocal EnableExtensions\npushd \"~dp0\"\n\npopd\n:EOF"))
 
 (defun cmd-help  (&optional arg)
   "Show help output for command in other window in view-mode.  Command 
